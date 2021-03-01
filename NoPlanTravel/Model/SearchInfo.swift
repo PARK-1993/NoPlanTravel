cd562ce5c152f0431a9cb933df96eda3c55cda32
PARK-1993 <69801427+PARK-1993@users.noreply.github.com>
2021-03-01 21:07:10 +0900

    Add : ViewControllers and Class Files
    Mod : Folder structure

---
+++ NoPlanTravel/Model/SearchInfo.swift
@@ -0,0 +1,8 @@
+//
+//  SearchInfo.swift
+//  NoPlanTravel
+//
+//  Created by Joonhyun park on 2021/03/01.
+//
+
+import Foundation
---
+++ NoPlanTravel/Model/TouristSpot.swift
@@ -0,0 +1,8 @@
+//
+//  TouristSpot.swift
+//  NoPlanTravel
+//
+//  Created by Joonhyun park on 2021/03/01.
+//
+
+import Foundation
